5375677c67c0f5f58628902707fa1ff19214c8d3
Hyunghwan Shin <hhshin@plutosolution.com>
2024-09-11 17:56:18 +0900

    poetry removed from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ ls -l
 
 # Run the database migration first
 echo "Running database migration..."
-poetry run poe db-push
+#poetry run poe db-push
+python3 -c "import asyncio; from src.app.db import init_models; asyncio.run(init_models())"
 
 # Run the commands in parallel
 echo "Starting the tailwind build and the main application in the background..."
-parallel --ungroup ::: "poetry run poe dev-tailwind" "poetry run uvicorn app.app:app --host 0.0.0.0 --reload"
+#parallel --ungroup ::: "poetry run poe dev-tailwind" "poetry run uvicorn app.app:app --host 0.0.0.0 --reload"
+parallel --ungroup ::: "tailwindcss -i static/input.css -o static/output.css --watch=always" "uvicorn src.app.app:app --host 0.0.0.0 --reload"
